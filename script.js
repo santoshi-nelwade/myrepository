9259a2579c8580c34156cb1a40bc03dce52f70a9
santoshi-nelwade <santoshinelwade@gmail.com>
2026-01-27 21:56:11 +0530

    add file

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
+
+function visitCafe() {
+    alert("Thank you for choosing Brew Bliss Café! ☕");
+}
